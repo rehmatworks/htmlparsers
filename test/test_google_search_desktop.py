81ffa9be73e57b55a3eb7e51989a2b43420a80dd
Rehmat Alam <contact@rehmat.works>
2021-06-23 02:36:43 +0500

    Run all tests

--- test/test_google_search_desktop.py
+++ test/test_google_search_desktop.py
@@ -30,81 +30,81 @@ class TestGoogleHtmlParser(unittest.TestCase):
         """
         self.client.close()
 
-    # def test__get_organic(self) -> None:
-    #     """Test organic results.
+    def test__get_organic(self) -> None:
+        """Test organic results.
 
-    #     Test organic results and ensure that the results are parsed correctly.
-    #     """
-    #     results = self.parser._get_organic()
+        Test organic results and ensure that the results are parsed correctly.
+        """
+        results = self.parser._get_organic()
 
-    #     # Ensure that the data is a list
-    #     self.assertEqual(type(results), list)
+        # Ensure that the data is a list
+        self.assertEqual(type(results), list)
 
-    #     # Confirm that the data has results
-    #     self.assertGreater(len(results), 1)
+        # Confirm that the data has results
+        self.assertGreater(len(results), 1)
 
-    # def test_results_has_url(self) -> None:
-    #     """Test URL.
+    def test_results_has_url(self) -> None:
+        """Test URL.
 
-    #     Test that the result dict has the URL value set.
-    #     """
-    #     results = self.parser._get_organic()
-    #     for result in results:
-    #         self.assertNotEqual(result.get('url'), None)
+        Test that the result dict has the URL value set.
+        """
+        results = self.parser._get_organic()
+        for result in results:
+            self.assertNotEqual(result.get('url'), None)
 
-    # def test_results_has_title(self) -> None:
-    #     """Test title.
+    def test_results_has_title(self) -> None:
+        """Test title.
 
-    #     Ensure that the result dict has the title value set.
-    #     """
-    #     results = self.parser._get_organic()
-    #     for result in results:
-    #         self.assertNotEqual(result.get('title'), None)
+        Ensure that the result dict has the title value set.
+        """
+        results = self.parser._get_organic()
+        for result in results:
+            self.assertNotEqual(result.get('title'), None)
     
-    # def test_results_has_snippet(self) -> None:
-    #     """Test meta description snippet.
+    def test_results_has_snippet(self) -> None:
+        """Test meta description snippet.
         
-    #     Ensure that the result dict has the snippet value set.
-    #     """
-    #     results = self.parser._get_organic()
-    #     for result in results:
-    #         self.assertNotEqual(result.get('snippet'), None)
+        Ensure that the result dict has the snippet value set.
+        """
+        results = self.parser._get_organic()
+        for result in results:
+            self.assertNotEqual(result.get('snippet'), None)
     
-    # def test__get_estimated_results(self) -> None:
-    #     """Test estimated results.
+    def test__get_estimated_results(self) -> None:
+        """Test estimated results.
         
-    #     Ensure that we get the estimated results count from Google as an integer.
-    #     """
-    #     estimated_results = self.parser._get_estimated_results()
-    #     self.assertEqual(type(estimated_results), int)
+        Ensure that we get the estimated results count from Google as an integer.
+        """
+        estimated_results = self.parser._get_estimated_results()
+        self.assertEqual(type(estimated_results), int)
     
-    # def test_get_data(self) -> None:
-    #     """Test final data.
+    def test_get_data(self) -> None:
+        """Test final data.
         
-    #     Ensure that we get the final data in form of a dictionary.
-    #     """
-    #     final_data = self.parser.get_data()
-    #     self.assertEqual(type(final_data), dict)
+        Ensure that we get the final data in form of a dictionary.
+        """
+        final_data = self.parser.get_data()
+        self.assertEqual(type(final_data), dict)
     
-    # def test_invalid_google_html(self) -> None:
-    #     """Test invalid HTML.
+    def test_invalid_google_html(self) -> None:
+        """Test invalid HTML.
         
-    #     Provide an invalid string as the HTML source and ensure that InvalidGoogleHtml exception is raised.
-    #     """
+        Provide an invalid string as the HTML source and ensure that InvalidGoogleHtml exception is raised.
+        """
         
-    #     html_str = """
-    #     <!DOCTYPE html>
-    #     <html>
-    #         <head>
-    #             <title>Invalid HTML source</title>
-    #         </head>
-    #         <body>
-    #             <p>This source does not contain any Google Search results.</p>
-    #         </body>
-    #     </html>
-    #     """
-    #     parser = GoogleHtmlParser(html_str)
-    #     self.assertRaises(InvalidGoogleHtml, parser._get_estimated_results)
+        html_str = """
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Invalid HTML source</title>
+            </head>
+            <body>
+                <p>This source does not contain any Google Search results.</p>
+            </body>
+        </html>
+        """
+        parser = GoogleHtmlParser(html_str)
+        self.assertRaises(InvalidGoogleHtml, parser._get_estimated_results)
     
     def test_featured_snippet(self) -> None:
         """Test featured snippet.
